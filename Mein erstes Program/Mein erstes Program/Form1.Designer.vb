351da14efe39b0894971a52e3b8037347966fb9e
abernhard <andy.bernhard@gmx.ch>
2020-03-25 18:55:03 +0100

    Button Andy hinzugefügt

--- Mein erstes Program/Mein erstes Program/Form1.Designer.vb
+++ Mein erstes Program/Mein erstes Program/Form1.Designer.vb
@@ -22,55 +22,66 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.cmdHallo = New System.Windows.Forms.Button()
-        Me.cmdEnde = New System.Windows.Forms.Button()
-        Me.lblAnzeige = New System.Windows.Forms.Label()
-        Me.SuspendLayout()
-        '
-        'cmdHallo
-        '
-        Me.cmdHallo.Location = New System.Drawing.Point(39, 87)
-        Me.cmdHallo.Name = "cmdHallo"
-        Me.cmdHallo.Size = New System.Drawing.Size(99, 39)
-        Me.cmdHallo.TabIndex = 0
-        Me.cmdHallo.Text = "Hallo"
-        Me.cmdHallo.UseVisualStyleBackColor = True
-        '
-        'cmdEnde
-        '
-        Me.cmdEnde.Location = New System.Drawing.Point(39, 170)
-        Me.cmdEnde.Name = "cmdEnde"
-        Me.cmdEnde.Size = New System.Drawing.Size(99, 41)
-        Me.cmdEnde.TabIndex = 1
-        Me.cmdEnde.Text = "Ende"
-        Me.cmdEnde.UseVisualStyleBackColor = True
-        '
-        'lblAnzeige
-        '
-        Me.lblAnzeige.AutoSize = True
-        Me.lblAnzeige.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblAnzeige.Location = New System.Drawing.Point(68, 28)
-        Me.lblAnzeige.Name = "lblAnzeige"
-        Me.lblAnzeige.Size = New System.Drawing.Size(41, 15)
-        Me.lblAnzeige.TabIndex = 2
-        Me.lblAnzeige.Text = "Label1"
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.lblAnzeige)
-        Me.Controls.Add(Me.cmdEnde)
-        Me.Controls.Add(Me.cmdHallo)
-        Me.Name = "Form1"
-        Me.Text = "Mein erstes Program"
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Me.cmdHallo = New System.Windows.Forms.Button()
+		Me.cmdEnde = New System.Windows.Forms.Button()
+		Me.lblAnzeige = New System.Windows.Forms.Label()
+		Me.btnAndy = New System.Windows.Forms.Button()
+		Me.SuspendLayout()
+		'
+		'cmdHallo
+		'
+		Me.cmdHallo.Location = New System.Drawing.Point(39, 87)
+		Me.cmdHallo.Name = "cmdHallo"
+		Me.cmdHallo.Size = New System.Drawing.Size(99, 39)
+		Me.cmdHallo.TabIndex = 0
+		Me.cmdHallo.Text = "Hallo"
+		Me.cmdHallo.UseVisualStyleBackColor = True
+		'
+		'cmdEnde
+		'
+		Me.cmdEnde.Location = New System.Drawing.Point(39, 170)
+		Me.cmdEnde.Name = "cmdEnde"
+		Me.cmdEnde.Size = New System.Drawing.Size(99, 41)
+		Me.cmdEnde.TabIndex = 1
+		Me.cmdEnde.Text = "Ende"
+		Me.cmdEnde.UseVisualStyleBackColor = True
+		'
+		'lblAnzeige
+		'
+		Me.lblAnzeige.AutoSize = True
+		Me.lblAnzeige.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.lblAnzeige.Location = New System.Drawing.Point(68, 28)
+		Me.lblAnzeige.Name = "lblAnzeige"
+		Me.lblAnzeige.Size = New System.Drawing.Size(41, 15)
+		Me.lblAnzeige.TabIndex = 2
+		Me.lblAnzeige.Text = "Label1"
+		'
+		'btnAndy
+		'
+		Me.btnAndy.Location = New System.Drawing.Point(174, 179)
+		Me.btnAndy.Name = "btnAndy"
+		Me.btnAndy.Size = New System.Drawing.Size(75, 23)
+		Me.btnAndy.TabIndex = 3
+		Me.btnAndy.Text = "Andy"
+		Me.btnAndy.UseVisualStyleBackColor = True
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(284, 261)
+		Me.Controls.Add(Me.btnAndy)
+		Me.Controls.Add(Me.lblAnzeige)
+		Me.Controls.Add(Me.cmdEnde)
+		Me.Controls.Add(Me.cmdHallo)
+		Me.Name = "Form1"
+		Me.Text = "Mein erstes Program"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
-    Friend WithEvents cmdHallo As System.Windows.Forms.Button
+	End Sub
+	Friend WithEvents cmdHallo As System.Windows.Forms.Button
     Friend WithEvents cmdEnde As System.Windows.Forms.Button
     Friend WithEvents lblAnzeige As System.Windows.Forms.Label
-
+    Friend WithEvents btnAndy As Button
 End Class
--- Mein erstes Program/Mein erstes Program/Form1.vb
+++ Mein erstes Program/Mein erstes Program/Form1.vb
@@ -7,4 +7,8 @@
     Private Sub cmdEnde_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdEnde.Click
         Me.Close()
     End Sub
+
+    Private Sub btnAndy_Click(sender As Object, e As EventArgs) Handles btnAndy.Click
+        lblAnzeige.Text = "Mein Name ist Andy :-) Hallo"
+    End Sub
 End Class
